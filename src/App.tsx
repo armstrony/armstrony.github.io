a98831694f8fbb8be9c0117533758c0fc8097a94
armstrony <developer.hafi@gmail.com>
2025-02-13 12:37:02 +0700

    Adding Nav and React type animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { TypeAnimation } from "react-type-animation";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="homepage container mx-auto">
+        <nav className="navbar relative flex flex-row px-16 py-10 border-b-0">
+          <div className="flex w-full justify-between relative">
+            <span className="font-semibold uppercase tracking-[4px]" id="hafie">
+              hafie
+            </span>
+            <div className="flex">
+              <a href="#" className="ml-10 capitalize" id="portofolio">
+                home
+              </a>
+              <a href="#" className="ml-10 capitalize" id="home">
+                Portofolio
+              </a>
+            </div>
+          </div>
+          {/* Underline */}
+          <div className="nav-border absolute bottom-0 left-0 w-full">
+            <div className="nav-border-line absolute left-[0] right-[0] mx-auto w-[calc(100%-6rem)] border-b-1 "></div>
+          </div>
+        </nav>
+
+        <main className="main px-16">
+          <section className="hero">
+            <h1 className="hero-title text-[84px] bg-gradient-to-r from-green-hero to-pink-hero bg-clip-text text-transparent font-bold h-lvh flex justify-center items-center">
+              <TypeAnimation
+                preRenderFirstString={true}
+                sequence={[
+                  "Hi, I’m Hafie—just a normal person who loves discovering new things in tehc...",
+                  1000,
+                  "Hi, I’m Hafie—just a normal person who loves discovering new things in business...",
+                  1000,
+                  "Hi, I’m Hafie—just a normal person who loves discovering new things in anime...",
+                  1000,
+                  "Hi, I’m Hafie—just a normal person who loves discovering new things in etc...",
+                  1000,
+                ]}
+                repeat={Infinity}
+              />
+            </h1>
+            {/* <h1
+              className="hero-title text-[84px] bg-gradient-to-r 
+            from-green-hero to-pink-hero 
+            bg-clip-text text-transparent font-bold h-dvh flex justify-center items-center"
+            >
+              Hi, I’m Hafie—just a normal person who loves discovering new
+              things in tehc.
+            </h1> */}
+          </section>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
