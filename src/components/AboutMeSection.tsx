5f860fcf3b3359b3b42dd8fdeae4f9938a0ec47e
Hafi <developer.hafi@gmail.com>
2025-07-01 09:12:26 +0700

    change about me section

--- src/components/AboutMeSection.tsx
+++ src/components/AboutMeSection.tsx
@@ -9,15 +9,15 @@ const AboutMeSection = () => {
         <div className="md:w-3/5 space-y-4 text-gray-400">
           <p>Hello, i’m Hafi!</p>
           <p>
-            I’m a self-taught front-end developer based in Indonesia. I can
-            develop responsive websites from scratch and raise them into modern
-            user-friendly web experiences.
+            I’m a self-taught front-end developer from Indonesia who builds
+            responsive websites from the ground up and transforms them into
+            modern, user-friendly experiences. Also I create iOS apps that
+            enhance user experiences and solve user needs.
           </p>
           <p>
-            Transforming my creativity and knowledge into a websites has been my
-            passion for over a year. I have been helping various clients to
-            establish their presence online. I always strive to learn about the
-            newest technologies and frameworks.
+            Bringing ideas to life through websites and iOS apps has been a
+            passion of mine. I’ve helped clients build their online presence and
+            enjoy learning new tools and technologies as I grow.
           </p>
         </div>
         <div className="md:w-2/5 flex justify-center">
