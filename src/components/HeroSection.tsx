ff47651fbf864747b82b03e8d7cb8b802f08cc81
Hafi <developer.hafi@gmail.com>
2025-07-01 09:12:00 +0700

    Change description hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,15 +8,20 @@ const HeroSection = () => {
     >
       <div className="mb-10 md:mb-0">
         <h1 className="text-4xl lg:text-7xl font-bold text-white leading-tight">
-          Hafi is a <span className="text-teal-400">iOS Developer</span> and{" "}
+          Hello! I'm a <span className="text-teal-400">iOS Developer</span> and{" "}
           <span className="text-teal-400">Web Dev</span> and{" "}
           <span className="text-teal-400">DevOps Enthusiast</span>
         </h1>
         <p className="mt-4 text-gray-400">
-          He Turning ideas into apps, websites, and pipelines.
+          I'm turning ideas into apps, websites, and pipelines.
         </p>
-        <button className="mt-8 px-6 py-3 border border-teal-400 text-white font-semibold rounded-md hover:bg-teal-400/10 transition-colors duration-300 flex items-center gap-2">
-          Contact me!! <Send size={18} />
+        <button className="mt-8 px-5 py-3 border border-teal-400 text-white font-semibold rounded-md hover:bg-teal-400/10 transition-colors duration-300 ">
+          <a
+            href="https://linkedin.com/in/mkhadafiess"
+            className="flex items-center gap-2"
+          >
+            Contact me!! <Send size={18} />
+          </a>
         </button>
       </div>
     </section>
